b78f676e76eb8b89f1b89d8bd54efbe3187801b3
ctellezesp <johndoe@example.com>
2019-09-21 08:42:31 -0500

    Models and Routed added

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const dogsRouter = require('./routes/dogs');
 
-mongoose.connect('mongodb://127.0.0.1:27017/petsDB');
+
+mongoose.connect('mongodb://127.0.0.1:27017/petsDB', {
+	useNewUrlParser: true, useUnifiedTopology: true
+});
 
 const app = express();
 
@@ -17,11 +20,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/resources', express.static(path.join(__dirname, 'public')));
 
 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dogs', dogsRouter);
+app.use('/cats', (req,res) => {
+	res.status(404).send("we don't like cats");
+});
+app.use((error, req, res, next) => {
+	if(error) {
+		res.status(422).json({
+			message: error.message,
+			type: error.name
+		})
+	}
+})
 module.exports = app;
--- routes/dogs.js
+++ routes/dogs.js
@@ -5,92 +5,105 @@ const mongoose = require('mongoose');
 
 //Creates model for Route
 const dogSchema = new mongoose.Schema({
-	_id: mongoose.Schema.Types.ObjectId,
-	name: String,
-	age: Number
+	//_id: mongoose.Schema.Types.ObjectId,
+	name: {
+		type: String,
+		required: [true, 'is required']
+	},
+	age: {
+		type: Number,
+		default: 0
+	}
 })
 const Dog = mongoose.model('dogs', dogSchema);
 
 // Endpoints
 // Index get /Dogs
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
 	Dog.find({})
 	.then(result => {
 		if(result.length)
-			res.status(200).send(result);
+			res.status(200).json({
+				message: "There are many dogs ",
+				dog: result
+			});
 		else 
 		res.status(404).send('There are no dogs');
 	})
-	.catch(error => {
-		res.status(500).send(error);
-	})
+	.catch(next);
 });
 
 //CREATE POST /dogs
-router.post('/', (req,res) => {
+router.post('/', (req, res, next) => {
 	const body = req.body;
-	const newDog = new Dog({
+	/*const newDog = new Dog({
 		_id: new mongoose.Types.ObjectId(),
 		...body
 	});
 
-	newDog.save()
+	newDog.save()*/
+	Dog.create(body)
 	.then(result => {
 		if(result)
-			res.status(201).send(result);
+			res.status(201).json({
+				message: "New dog Created",
+				dog: result
+			});
 		else 
-		res.status(404).send('Cant create dog');
+		res.status(404).json({
+			message: "Can't created dog"
+		});
 	})
-	.catch(error => {
-		res.status(500).send(error);
-	})
+	.catch(next);
 });
 
 //GET - Dog
-router.get('/:id', (req,res) => {
+router.get('/:id', (req, res, next) => {
 	let id = req.params.id;
-	Dog.findById(id).exec()
+	Dog.findById(id)
 	.then(result => {
 		if(result) {
-			res.status(200).send(result);
+			res.status(200).json({
+				message: "You find the dog",
+				dog: result
+			});
 		} else {
 			res.status(404).send('dog not found!');
 		}
 	})
-	.catch(err => {
-		res.status(500).send('Cant fetch dog');
-	});
+	.catch(next);
 });
 
 //PUT - Dogs
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
 	let id = req.params.id;
 	let body = req.body;
 
 	Dog.findByIdAndUpdate(id, body, {new: true})
 	.then(result => {
 		if(result) {
-			res.status(200).send(result);
+			res.status(200).json({
+				message: "Dog modificated",
+				dog: result
+			});
 		} else {
 			res.status(404).send('Cant update, this dog is missing');
 		}
 	})
-	.catch(error => {
-		res.status(500).send(error);
-	});
+	.catch(next);
 });
 
 //DELETE - DOGS
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
 	let id = req.params.id;
 	Dog.findByIdAndRemove(id)
 	.then(() => {
-		res.status(204).send();
+		res.status(204).json({
+			message: "Dog Deleted correctly"
+		});
 	})
-	.catch(error => {
-		res.status(500).send(error);
-	})
-})
+	.catch(next);
+});
 
 module.exports = router;
